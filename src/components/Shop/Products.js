fc3b77b8ce1b82215ce8eaa6c5344c4dbda2aa79
triptocode <3580rio@gmail.com>
2023-04-16 08:48:55 +0900

    두번째 상품추가 구현

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -1,16 +1,35 @@
 import ProductItem from './ProductItem';
 import classes from './Products.module.css';
 
+const DUMMY_PRODUCTS = [
+  {
+    id: 'p1',
+    price: 6,
+    title: 'My First Book',
+    description: 'The first book I ever wrote',
+  },
+  {
+    id: 'p2',
+    price: 5,
+    title: 'My Second Book',
+    description: 'The second book I ever wrote',
+  },
+];
+
 const Products = (props) => {
   return (
     <section className={classes.products}>
-      <h2> 구매할 상품 카트에 담기</h2>
+      <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title='a book'
-          price={5}
-          description='this is a first product!'
-        />
+        {DUMMY_PRODUCTS.map((product) => (
+          <ProductItem
+            key={product.id}
+            id={product.id}
+            title={product.title}
+            price={product.price}
+            description={product.description}
+          />
+        ))}
       </ul>
     </section>
   );
